9b60b9f88ca09fe4f6959c78ea5582240718fbb8
roman.cf <roman.springer@outlook.com>
2019-10-07 15:15:42 +0200

    work in prgress

--- challenge/js/main.js
+++ challenge/js/main.js
@@ -1,8 +1,10 @@
 var productText = ["Schuhe Null", "Schuh Eins","Schuh Zwei","Schuh Drei"];
-var productPrice= [39.99, 75.00, 69.99, 120.00];
+var productPrice= [40.00, 75.00, 69.99, 120.00];
 
 var cartArr = [];
 var addCount = 0
+var sum = 0
+
 
 function addFunction(pId){
 	
@@ -11,6 +13,9 @@ function addFunction(pId){
 		let cartZeile = document.createElement("li");
 			cartZeile.id = "liId"+addCount;
 		let protuctID = document.createTextNode(productText[pId]);
+		let cartRight = document.createElement("div");
+
+		let	cartPrice = document.createTextNode((productPrice[pId]).toFixed(2)+"€");		
 		let cartBtn = document.createElement("BUTTON");
 			cartBtn.innerHTML = "x";
 			cartBtn.id= "liId"+addCount;
@@ -19,15 +24,29 @@ function addFunction(pId){
 				}
 			
 			cartZeile.appendChild(protuctID);
-			cartZeile.appendChild(cartBtn);
+			cartZeile.appendChild(cartRight);
+			cartRight.appendChild(cartPrice);
+			cartRight.appendChild(cartBtn);
 
 			document.getElementById("cartList").appendChild(cartZeile);
 	addCount ++;
-	
+
+	Summme();
 }
+
 
 function CartZeileDelete(x){
 	console.log(x);
 	let deletE = document.getElementById(x);
 	deletE.parentNode.removeChild(deletE);
+	Summme();
+}
+
+
+function Summme(){
+	sum = 0
+	for (i=0; i < cartArr.length; i++){
+		sum = sum + productPrice[cartArr[i]];
+	}
+	document.getElementById("summ").innerHTML = sum;
 }
