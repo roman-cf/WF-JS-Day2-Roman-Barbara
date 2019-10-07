227b041f495fe75348e65ae5ec0dbee94f215a28
Barbara Anissin <anissinb@gmail.com>
2019-10-07 16:01:29 +0200

    done

--- challenge/js/main.js
+++ challenge/js/main.js
@@ -1,52 +1,57 @@
-var productText = ["Schuhe Null", "Schuh Eins","Schuh Zwei","Schuh Drei"];
-var productPrice= [40.00, 75.00, 69.99, 120.00];
+var productText = ["Schuhe Null", "Schuh Eins", "Schuh Zwei", "Schuh Drei"];
+var productPrice = [40.00, 75.00, 69.99, 120.00];
 
 var cartArr = [];
 var addCount = 0
 var sum = 0
 
 
-function addFunction(pId){
-	
-	cartArr.push(pId)
+function addFunction(pId) {
 
-		let cartZeile = document.createElement("li");
-			cartZeile.id = "liId"+addCount;
-		let protuctID = document.createTextNode(productText[pId]);
-		let cartRight = document.createElement("div");
+    cartArr.push(pId)
 
-		let	cartPrice = document.createTextNode((productPrice[pId]).toFixed(2)+"€");		
-		let cartBtn = document.createElement("BUTTON");
-			cartBtn.innerHTML = "x";
-			cartBtn.id= "liId"+addCount;
-			cartBtn.onclick = function (e){
-				CartZeileDelete(e.target.id);
-				}
-			
-			cartZeile.appendChild(protuctID);
-			cartZeile.appendChild(cartRight);
-			cartRight.appendChild(cartPrice);
-			cartRight.appendChild(cartBtn);
+    let cartZeile = document.createElement("li");
+    cartZeile.id = "liId" + addCount;
+    let protuctID = document.createTextNode(productText[pId]);
+    let cartRight = document.createElement("div");
 
-			document.getElementById("cartList").appendChild(cartZeile);
-	addCount ++;
+    let cartPrice = document.createTextNode((productPrice[pId]).toFixed(2) + "€");
+    let cartBtn = document.createElement("BUTTON");
+    cartBtn.innerHTML = "x";
+    cartBtn.id = "liId" + addCount;
+    cartBtn.onclick = function(e) {
+        CartZeileDelete(e.target.id);
+    }
 
-	Summme();
+    cartZeile.appendChild(protuctID);
+    cartZeile.appendChild(cartRight);
+    cartRight.appendChild(cartPrice);
+    cartRight.appendChild(cartBtn);
+
+    document.getElementById("cartList").appendChild(cartZeile);
+    addCount++;
+
+    console.log(cartArr);
+    Summe();
 }
 
 
-function CartZeileDelete(x){
-	console.log(x);
-	let deletE = document.getElementById(x);
-	deletE.parentNode.removeChild(deletE);
-	Summme();
+function CartZeileDelete(x) {
+
+    console.log(x);
+    let deletE = document.getElementById(x);
+    deletE.parentNode.removeChild(deletE);
+    cartArr.splice(x.slice(4), 1, "x");
+    Summe();
 }
 
 
-function Summme(){
-	sum = 0
-	for (i=0; i < cartArr.length; i++){
-		sum = sum + productPrice[cartArr[i]];
-	}
-	document.getElementById("summ").innerHTML = sum;
+function Summe() {
+    sum = 0
+    for (i = 0; i < cartArr.length; i++) {
+        if (cartArr[i] != "x") {
+            sum = Number(((sum) + productPrice[cartArr[i]]).toFixed(2));
+        } 
+    }
+    document.getElementById("summ").innerHTML = sum;
 }
