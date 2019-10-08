6569dcbaec0f423ba243928abf2efcd23195900f
roman.cf <roman.springer@outlook.com>
2019-10-08 09:34:20 +0200

    work in prgress

--- challenge/js/main.js
+++ challenge/js/main.js
@@ -1,14 +1,24 @@
 var productText = ["Schuhe Null", "Schuh Eins", "Schuh Zwei", "Schuh Drei"];
 var productPrice = [40.00, 75.00, 69.99, 120.00];
 
-var cartArr = [];
+var cartArr = [[],[]];
 var addCount = 0
 var sum = 0
 
 
 function addFunction(pId) {
+    var stueck = 1;
 
-    cartArr.push(pId)
+    console.log(cartArr[0].indexOf(pId));
+    console.log(cartArr[1][cartArr[0].indexOf(pId)]);
+
+    cartArr[0].push(pId);
+    cartArr[1].push(stueck);
+
+    //console.log(pId);
+    console.log(cartArr);
+   
+
 
     let cartZeile = document.createElement("li");
     cartZeile.id = "liId" + addCount;
@@ -31,27 +41,26 @@ function addFunction(pId) {
     document.getElementById("cartList").appendChild(cartZeile);
     addCount++;
 
-    console.log(cartArr);
     Summe();
 }
 
 
 function CartZeileDelete(x) {
 
-    console.log(x);
     let deletE = document.getElementById(x);
-    deletE.parentNode.removeChild(deletE);
-    cartArr.splice(x.slice(4), 1, "x");
+        deletE.parentNode.removeChild(deletE);
+        cartArr[0].splice(x.slice(4), 1, "x");
+        cartArr[1].splice(x.slice(4), 1, "x");
     Summe();
 }
 
 
 function Summe() {
     sum = 0
-    for (i = 0; i < cartArr.length; i++) {
-        if (cartArr[i] != "x") {
-            sum = Number(((sum) + productPrice[cartArr[i]]).toFixed(2));
+    for (i = 0; i < cartArr[0].length; i++) {
+        if (cartArr[0][i] != "x") {
+            sum = Number(((sum) + productPrice[cartArr[0][i]]).toFixed(2));
         } 
     }
-    document.getElementById("summ").innerHTML = sum;
+    document.getElementById("summ").innerHTML = sum.toFixed(2)+"€";
 }
